7a2dc530c90944b405ad214e971168dee3ec7ac0
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2025-04-14 18:52:22 +0900

    Create alphabet method to be able to use freely

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,2 +1,11 @@
+def alphabet
+  @alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+end
 
+def caesar_cipher(string, shift)
+  string.split('')
 
+  shift = index + shift
+end
+
+p caesar_cipher("hello", 5)
