d35d1e9f0d7b398342676cc53eceb213d1e41282
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2025-04-14 21:43:41 +0900

    Add files via upload

---
+++ caesar_cipher.rb
@@ -0,0 +1,16 @@
+def alphabet
+  ('a'..'z').to_a
+end
+
+def caesar_cipher(string, shift)
+  string.split('').map do |char|
+    if alphabet.include?(char)
+      new_index = (alphabet.index(char) + shift) % 26
+      alphabet[new_index]
+    else
+      char
+    end
+  end.join
+end
+
+p caesar_cipher('hello baby!', 5)
