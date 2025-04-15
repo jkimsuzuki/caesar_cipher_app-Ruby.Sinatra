efef7b07fb2e5b8b6c4f0ba859542ab20b4018bd
jkimsuzuki <161112142+jkimsuzuki@users.noreply.github.com>
2025-04-15 13:43:06 +0900

    Create public folder with styles.css and views folder with index.erb to add HTML.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ruby_projects
+Lightweight Ruby/Sinatra Full-Stack Web App
+
+First basic Ruby project through The Odin Project course. 
+
+Created and completed caesar_cipher.rb first.
+
+Then my next steps was to see if I could make it full stack.
+
+I downloaded Sinatra as the framework to get my first basic full-stack app running. - app.rb
+
+Then created index.erb to have a more compartmentalized structure to write out my HTML. views/index.erb
+
+Next, in a public folder, created styles.css to stylize the page. public/styles.css
--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
 
+def caesar_cipher(string, shift)
+  lowercase = ('a'..'z').to_a
+  uppercase = ('A'..'Z').to_a
+
+  string.split('').map do |char|
+    if lowercase.include?(char)
+      new_index = (lowercase.index(char) + shift) % 26
+      lowercase[new_index]
+    elsif uppercase.include?(char)
+      new_index = (uppercase.index(char) + shift) % 26
+      uppercase[new_index]
+    else
+      char
+    end
+  end.join
+end
+
 get '/' do
-  'Hello my name is Joe'
+  erb :index
+end
+
+post '/cipher' do
+  text = params[:text]
+  shift = params[:shift].to_i
+  @result = caesar_cipher(text, shift)
+  erb :index
 end
